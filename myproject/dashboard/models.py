8799b1051a7844ff7f906db43a2adf3a4b847cb2
Bahar Işıklı <baharisikli@outlook.com>
2025-04-04 14:25:37 +0300

    status table model added to dashboard

--- myproject/dashboard/models.py
+++ myproject/dashboard/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from users.models import User
+
 
 class Book(models.Model):
     name = models.CharField(max_length=255)
@@ -7,3 +9,20 @@ class Book(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Status(models.Model):
+    STATUS_CHOICES = [
+        ('Okudum', 'Okudum'),
+        ('Okuyorum', 'Okuyorum'),
+        ('Okuyacağım', 'Okuyacağım'),
+    ]
+
+    userid = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=100)
+    bookid = models.ForeignKey(Book, on_delete=models.CASCADE)
+    bookname = models.CharField(max_length=255)
+    bookpage = models.IntegerField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.username} - {self.bookname} - {self.status}"
